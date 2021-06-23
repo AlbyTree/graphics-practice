b3cc9178b6f24c0de27e76676c587284442efb00
a.grimaudo <albyprogramming@gmail.com>
2021-06-23 18:06:16 +0200

    Remove copy-pasted test class code
    Removed some class members and code used inside another test class which
    I copy-pasted as an initial draft for the projection class.

--- src/tests/TestProj.cpp
+++ src/tests/TestProj.cpp
@@ -14,42 +14,6 @@ namespace test
 			m_n(-2.0f), m_f(-10.0f),
 			m_point_p(glm::vec3()), m_point_c(glm::vec4()), m_point_NDC(glm::vec3())
 	{
-        float positions[] = {
-            // 1st Triangle of square face
-            // Positions    // Texture Positions
-            -50.0f, -50.0f,   0.0f, 0.0f, // 0 
-             50.0f, -50.0f,   1.0f, 0.0f, // 1
-             50.0f,  50.0f,   1.0f, 1.0f, // 2
-            // Unique vertices for a square
-            -50.0f,  50.0f,   0.0f, 1.0f // 3
-        };
-
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 0, 3
-        };
-
-        // Allow to use the alpha pixel values to "blend" the pixels
-        // i.e. to reduce opacity of those pixels on top of the pixels with alpha values.
-        GLCall(glEnable(GL_BLEND));
-        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        m_VAO = std::make_unique<VertexArray>();
-        m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
-        VertexBufferLayout vbLayout;
-        // Vertex Positions
-        vbLayout.Push<float>(2);
-        // Texture Positions
-        vbLayout.Push<float>(2);
-        m_VAO->AddBuffer(*m_VertexBuffer, vbLayout);
-        m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 6);
-
-        m_Shader = std::make_unique<Shader>("res/shaders/Basic.shader");
-        m_Shader->Bind();
-        m_Shader->SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-
-        m_Texture = std::make_unique<Texture>("res/textures/ChernoLogo.png");
-        m_Shader->SetUniform1i("u_Texture", 0);
 	}
 
 	TestPerspProjSteps::~TestPerspProjSteps()
@@ -65,35 +29,10 @@ namespace test
 		GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-        Renderer renderer;
-
-        m_Texture->Bind();
-
-        {
-            // Move object to the x-left from its initial position.
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), m_TranslationA);
-            glm::mat4 mvp = m_Proj * m_View * model;
-            m_Shader->Bind();
-            m_Shader->SetUniformMat4f("u_MVP", mvp);
-
-            renderer.Draw(*m_VAO, *m_IndexBuffer, *m_Shader);
-        }
-
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), m_TranslationB);
-            glm::mat4 mvp = m_Proj * m_View * model;
-            m_Shader->Bind();
-            m_Shader->SetUniformMat4f("u_MVP", mvp);
-
-            renderer.Draw(*m_VAO, *m_IndexBuffer, *m_Shader);
-        }
 	}
 
 	void TestPerspProjSteps::OnImGuiRenderer()
 	{
-        ImGui::SliderFloat3("Translation A", &m_TranslationA.x, 0.0f, 960.0f);
-        ImGui::SliderFloat3("Translation B", &m_TranslationB.x, 0.0f, 960.0f);
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 	}
 
     void TestPerspProjSteps::ProjectPointToNearPlane()
--- src/tests/TestProj.h
+++ src/tests/TestProj.h
@@ -39,14 +39,5 @@ namespace test
 
 		// Perspective projection planes parameters
 		float m_l, m_r, m_t, m_b, m_n, m_f;
-
-		std::unique_ptr<VertexArray> m_VAO;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<Texture> m_Texture;
-
-		glm::mat4 m_Proj, m_View;
-		glm::vec3 m_TranslationA, m_TranslationB;
 	};
 }
