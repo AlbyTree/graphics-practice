b9d30ce66f1acf1c81cb008bc5424c60a6b9edef
a.grimaudo <albyprogramming@gmail.com>
2021-08-03 12:43:51 +0200

    Re-write the entire projection test class
    Instead of just trying to test the projection process with a point
    (which was useless) now I use a triangle.
    I added a lot of info on screen about the vertices and their transformed
    positions.
    I can also move and resize the vertices.
    Finally, I can switch between perspective and orthographic projection.
    At the moment, perspective projection is possible only with the 4 planes
    parameters and not with the FOV and aspect ratio parameters.

--- src/tests/TestProj.cpp
+++ src/tests/TestProj.cpp
@@ -1,5 +1,6 @@
 #include "TestProj.h"
 #include "../core/Renderer.h"
+#include "../core/Utils.h"
 
 #include "../deps/imgui/imgui.h"
 #include "../deps/glm/glm.hpp"
@@ -8,25 +9,27 @@
 
 namespace test
 {
-	TestPerspProjSteps::TestPerspProjSteps()
-        : m_point_e(glm::vec3(0.0f, 0.0f, -2.0f)), 
-			m_l(-470.0f), m_r(470.0f), 
-			m_t(270.0f), m_b(-270.0f), 
-			m_n(1.0f), m_f(5.0f),
-			m_point_p(glm::vec3()), m_point_c(glm::vec4()), m_point_NDC(glm::vec3())
+    TestPerspProjSteps::TestPerspProjSteps()
+        : m_UpdatedData(),
+        m_ProjectedData(), m_ClippedData(), m_NDCData(),
+        m_r(470.0f), m_l(-470.0f),
+        m_t(270.0f), m_b(-270.0f),
+        m_n(1.0f), m_f(5.0f),
+        m_fov(glm::radians(45.0f)), m_ar(960.0f / 540.0f),
+        m_Projection(), m_TypeOfProj(PERSPECTIVE_PROJ),
+        m_Translation(0.0f), m_Resize(1.0f), m_ScalingFactor(1.0f)
 	{
-        unsigned int indices[] = {0};
-
-        GLCall(glEnable(GL_PROGRAM_POINT_SIZE));
+        m_Data = compgraphutils::CreateTrianglePositions();
+        unsigned int indices[] = {0, 1, 2};
 
         m_VAO = std::make_unique<VertexArray>();
-        m_VertexBuffer = std::make_unique<VertexBuffer>(glm::value_ptr(m_point_NDC), 3*sizeof(float));
+        m_VertexBuffer = std::make_unique<VertexBuffer>(glm::value_ptr(m_Data), sizeof(m_Data));
 
         VertexBufferLayout vbLayout;
         vbLayout.AddAttrib<float>(3);
 
         m_VAO->AddBuffer(*m_VertexBuffer, vbLayout, true);
-        m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 1);
+        m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 3);
 
         m_Shader = std::make_unique<Shader>("res/shaders/PerspProjSteps.shader");
 	}
@@ -46,22 +49,47 @@ namespace test
 
         Renderer renderer;
 
-        ProjectPointToNearPlane();
-        MapPointToClipSpace();
-        NDCTransf();
+        // Resize and then translate vertices positions
+        glm::mat4 UpdatePosition = glm::translate(glm::mat4(1.0f), m_Translation);
+        m_Resize = glm::vec2(m_ScalingFactor);
+        UpdatePosition = glm::scale(UpdatePosition, glm::vec3(m_Resize, 1.0f));
+        for (int i = 0; i < 3; i++)
+			m_UpdatedData[i] = UpdatePosition * glm::vec4(m_Data[i], 1.0f);
+
+        // Compute vertices positions projection application side
+        for (int i = 0; i < 3; i++)
+            m_ProjectedData[i] = m_TypeOfProj ?
+            compgraphutils::ProjectPointToNearPlane(m_UpdatedData[i], m_n)
+            : compgraphutils::ProjectPointToNearPlane(m_UpdatedData[i], m_n, false);
+
+        for (int i = 0; i < 3; i++)
+            m_ClippedData[i] = m_TypeOfProj ?
+            compgraphutils::MapPointToClipSpace(m_UpdatedData[i], m_r, m_l, m_t, m_b, m_n, m_f)
+            : compgraphutils::MapPointToClipSpace(m_UpdatedData[i], m_r, m_l, m_t, m_b, m_n, m_f, false);
+
+        for (int i = 0; i < 3; i++)
+            m_NDCData[i] = compgraphutils::NDCTransf(m_ClippedData[i]);
+
+        // Compute projection matrix for GPU side
+        m_Projection = m_TypeOfProj ?
+		compgraphutils::CreatePerspectiveMatrix(m_r, m_l, m_t, m_b, m_n, m_f)
+		: compgraphutils::CreateOrthoProjMatrix(m_r, m_l, m_t, m_b, m_n, m_f);
+
+        glm::mat4 MVP = m_Projection * UpdatePosition;
+        //glm::mat4 MVP = glm::perspective(m_fov, m_ar, m_n, m_f);
 
 		m_Shader->Bind();
-		m_Shader->SetUniform3fv("u_NDCPos", 1, glm::value_ptr(m_point_NDC));
+        m_Shader->SetUniformMat4f("u_MVP", MVP);
 
-		renderer.Draw(*m_VAO, *m_IndexBuffer, *m_Shader, POINTS);
+		renderer.Draw(*m_VAO, *m_IndexBuffer, *m_Shader);
 	}
 
 	void TestPerspProjSteps::OnImGuiRenderer()
 	{
-        ImGui::Text("Eye Space Point: ");
-        ImGui::SliderFloat2("x,y coordinates", &m_point_e.x, -1000.0f, 1000.0f);
-        ImGui::PushItemWidth(120.0f);
-        ImGui::InputFloat("z coordinate", &m_point_e.z, 0.05f);
+        ImGui::SliderFloat("Translate X", &m_Translation.x, -20.0f, 20.0f);
+        ImGui::SliderFloat("Translate Y", &m_Translation.y, -20.0f, 20.0f);
+        ImGui::SliderFloat("Translate Z", &m_Translation.z, -20.0f, 20.0f);
+        ImGui::SliderFloat("Resize", &m_ScalingFactor, 0.0f, 100.0f);
 
         ImGui::Text("PROJ PLANES PARAMS: \n");
 
@@ -77,43 +105,42 @@ namespace test
         ImGui::PushItemWidth(120.0f);
         ImGui::InputFloat("bottom", &m_b, 0.5f);
 
+        //ImGui::PushItemWidth(120.0f);
+        //ImGui::InputFloat("fov,", &m_fov, 0.5f);
+        //ImGui::SameLine();
+        //ImGui::PushItemWidth(120.0f);
+        //ImGui::InputFloat("aspect ratio", &m_ar, 0.5f);
+
         ImGui::PushItemWidth(120.0f);
         ImGui::InputFloat("near,", &m_n, 0.05f);
         ImGui::SameLine();
         ImGui::PushItemWidth(120.0f);
         ImGui::InputFloat("far", &m_f, 0.05f);
 
-        ImGui::Text("Projected Point: [%f,%f,%f]", m_point_p.x, m_point_p.y, m_point_p.z);
-        ImGui::Text("Clipped Point: [%f,%f,%f,%f]", m_point_c.x, m_point_c.y, m_point_c.z, m_point_c.w);
-        ImGui::Text("NDC Point: [%f,%f,%f]", m_point_NDC.x, m_point_NDC.y, m_point_NDC.z);
+        ImGui::Text("VERTICES: \n");
+        ImGui::Text("V1: [%f,%f,%f]", m_UpdatedData[0].x, m_UpdatedData[0].y, m_UpdatedData[0].z);
+        ImGui::Text("V2: [%f,%f,%f]", m_UpdatedData[1].x, m_UpdatedData[1].y, m_UpdatedData[1].z);
+        ImGui::Text("V3: [%f,%f,%f]", m_UpdatedData[2].x, m_UpdatedData[2].y, m_UpdatedData[2].z);
+
+        ImGui::Text("Proj V1: [%f,%f,%f]", m_ProjectedData[0].x, m_ProjectedData[0].y, m_ProjectedData[0].z);
+        ImGui::Text("Proj V2: [%f,%f,%f]", m_ProjectedData[1].x, m_ProjectedData[1].y, m_ProjectedData[1].z);
+        ImGui::Text("Proj V3: [%f,%f,%f]", m_ProjectedData[2].x, m_ProjectedData[2].y, m_ProjectedData[2].z);
+
+        ImGui::Text("Clip V1: [%f,%f,%f]", m_ClippedData[0].x, m_ClippedData[0].y, m_ClippedData[0].z);
+        ImGui::Text("Clip V2: [%f,%f,%f]", m_ClippedData[1].x, m_ClippedData[1].y, m_ClippedData[1].z);
+        ImGui::Text("Clip V3: [%f,%f,%f]", m_ClippedData[2].x, m_ClippedData[2].y, m_ClippedData[2].z);
+
+        ImGui::Text("NDC V1: [%f,%f,%f]", m_NDCData[0].x, m_NDCData[0].y, m_NDCData[0].z);
+        ImGui::Text("NDC V2: [%f,%f,%f]", m_NDCData[1].x, m_NDCData[1].y, m_NDCData[1].z);
+        ImGui::Text("NDC V3: [%f,%f,%f]", m_NDCData[2].x, m_NDCData[2].y, m_NDCData[2].z);
+
+		if (ImGui::Button("Perspective Proj"))
+		{
+            m_TypeOfProj = PERSPECTIVE_PROJ;
+		}
+		if (ImGui::Button("Orthographic Proj"))
+		{
+            m_TypeOfProj = ORTHO_PROJ;
+		}
 	}
-
-    void TestPerspProjSteps::ProjectPointToNearPlane()
-    {
-        m_point_p.x = (m_n * m_point_e.x) / (-m_point_e.z);
-        m_point_p.y = (m_n * m_point_e.y) / (-m_point_e.z);
-        m_point_p.z = m_n;
-    }
-
-    void TestPerspProjSteps::MapPointToClipSpace()
-    {
-        m_point_c.x = ((2 * m_n * m_point_e.x) / (m_r - m_l)) + (((m_r + m_l) / (m_r - m_l))*m_point_e.z);
-        m_point_c.y = ((2 * m_n * m_point_e.y) / (m_t - m_b)) + (((m_t + m_b) / (m_t - m_b))*m_point_e.z);
-        m_point_c.z = (((m_f + m_n) / (m_f - m_n)) * (-m_point_e.z)) - ((2 * m_f * m_n) / (m_f - m_n));
-        m_point_c.w = -m_point_e.z;
-    }
-
-    void TestPerspProjSteps::NDCTransf()
-    {
-        m_point_NDC.x = m_point_c.x / m_point_c.w;
-        m_point_NDC.y = m_point_c.y / m_point_c.w;
-        m_point_NDC.z = m_point_c.z / m_point_c.w;
-    }
-
-    void TestPerspProjSteps::ProjectPoint()
-    {
-        ProjectPointToNearPlane();
-        MapPointToClipSpace();
-        NDCTransf();
-    }
 }
--- src/tests/TestProj.h
+++ src/tests/TestProj.h
@@ -20,26 +20,31 @@ namespace test
 		void OnRenderer() override;
 		void OnImGuiRenderer() override;
 
-		void ProjectPointToNearPlane();
-		void MapPointToClipSpace();
-		void NDCTransf();
-		void ProjectPoint();
-
 	private:
 
-		// In the following order: 
-		// The point in eye space
-		// The point projected on the near plane
-		// The point clipped in homogeneous space 
-		// The point in NDC space
-		glm::vec3 m_point_e;
-		glm::vec3 m_point_p;
-		glm::vec4 m_point_c;
-		glm::vec3 m_point_NDC;
+		glm::mat3 m_Data;
+		// Eye-Space data stored application side
+		glm::mat3 m_UpdatedData;
 
 		// Perspective projection planes parameters:
 		// the near and far plane parameters MUST be positive values
 		float m_l, m_r, m_t, m_b, m_n, m_f;
+		// Perspective projection parameters
+		float m_fov, m_ar;
+		glm::mat4 m_Projection;
+
+		enum TypeOfProjection { ORTHO_PROJ = 0, PERSPECTIVE_PROJ = 1};
+		TypeOfProjection m_TypeOfProj;
+
+		// "Projection" intermediate transformed data stored application side
+		glm::mat3 m_ProjectedData;
+		glm::mat3x4 m_ClippedData; 
+		glm::mat3 m_NDCData;
+
+		// Additional transformations
+		glm::vec3 m_Translation;
+		glm::vec2 m_Resize;
+		float m_ScalingFactor;
 
 		std::unique_ptr<VertexArray> m_VAO;
 		std::unique_ptr<VertexBuffer> m_VertexBuffer;
