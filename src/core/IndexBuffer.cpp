fdc40a5e3dbcb42b845ed5dadd5f0a0ec2fc5181
albytree <albyprogramming@gmail.com>
2023-04-03 19:08:39 +0200

    Add buffers support for sending data later
    *  Added the bind/unbinding control directly in the buffers classes

--- src/core/IndexBuffer.cpp
+++ src/core/IndexBuffer.cpp
@@ -10,6 +10,36 @@ IndexBuffer::IndexBuffer(const unsigned int* data, unsigned int count)
     GLCall(glGenBuffers(1, &m_RendererID));
     GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
     GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW));
+	m_Init = true;
+	Unbind();
+}
+
+IndexBuffer::IndexBuffer()
+{
+    // If by chance, OpenGL unsigned int is not the usual 4 bytes...halt the execution.
+    ASSERT(sizeof(unsigned int) == sizeof(GLuint))
+
+    GLCall(glGenBuffers(1, &m_RendererID));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID));
+	m_Count = 0;
+	m_Init = false;
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+}
+
+void IndexBuffer::ReplaceBufferData(const void* data, unsigned int count)
+{
+	Bind();
+	if (!m_Init)
+	{
+		GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(unsigned int), data, GL_STATIC_DRAW))
+		m_Init = true;
+	}
+	else
+	{
+		GLCall(glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, count, data))
+	}
+	m_Count = count;
+	Unbind();
 }
 
 IndexBuffer::~IndexBuffer()
--- src/core/VertexBuffer.h
+++ src/core/VertexBuffer.h
@@ -9,6 +9,7 @@ public:
 	// Creates a buffer on the GPU and immediatly transfer data into it.
 	// `size` is the amount of bytes to send.
 	VertexBuffer(const void* data, unsigned int size);
+	VertexBuffer();
 	
 	// Creates a buffer on the GPU with `size` bytes left uninitialized.
 	VertexBuffer(unsigned int size);
