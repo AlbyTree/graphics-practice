488d8d36f604eb659d4533201c8defc2909bcd39
albytree <albyprogramming@gmail.com>
2023-04-03 19:11:25 +0200

    Rewrite the Mesh class
    *  Added vectors for attributes instead of single vector of attributes
    *  Added functions to modify the mesh attributes and query their
       values
    *  Added a deferred initialization of the data on the GPU
    
    Now the mesh can be edited and can update its buffers on the GPU.
    When defining the mesh, each vertex needs the same number of attributes
    values i.e. the same number of positions, colors, etc...

--- src/core/Mesh.h
+++ src/core/Mesh.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 
+#include <iterator>
 #include <vector>
 #include <memory>
 #include "VertexBufferLayout.h"
@@ -24,21 +25,49 @@ public:
     Mesh();
     ~Mesh() = default;
 
-    std::vector<glm::vec3> GetPositions();
-    inline unsigned int GetNumVertices() const { return m_Data.size(); };
-    void SetVertices(std::vector<MeshData>&& data);
-    void SetIndices(std::vector<unsigned int>&& indices);
-    void SetVertexColor(unsigned int index, const glm::vec4& c);
-
     inline const VertexArray& GetVAO() const { return *m_VAO; }
     inline const IndexBuffer& GetIB() const { return *m_IB; }
 
+	inline void AddPosition(const glm::vec3& p) { m_Positions.emplace_back(p); }
+	inline void AddPositions(const std::vector<glm::vec3>& p)
+    {
+	    std::copy(p.begin(), p.end(), std::back_insert_iterator<std::vector<glm::vec3>>(m_Positions));
+    }
+	inline void AddColor(const glm::vec4& c) { m_Colors.emplace_back(c); }
+	inline void AddColors(const std::vector<glm::vec4>& c)
+	{
+		std::copy(c.begin(), c.end(), std::back_insert_iterator<std::vector<glm::vec4>>(m_Colors));
+	}
+	inline void AddIndex(const unsigned int& i) { m_Indices.emplace_back(i); }
+	inline void AddIndices(const std::vector<unsigned int>& i)
+	{
+		std::copy(i.begin(), i.end(), std::back_insert_iterator<std::vector<unsigned int>>(m_Indices));
+	}
+
+	void SetVertexPosition(unsigned int i, const glm::vec3& p);
+	void SetVertexColor(unsigned int i, const glm::vec4& c);
+	void SetIndex(unsigned int i, unsigned int index);
+	void SetVerticesPositions(const std::vector<unsigned int>& range, const std::vector<glm::vec3>& p);
+	void SetVerticesColors(const std::vector<unsigned int>& range, const std::vector<glm::vec4>& c);
+	void SetIndices(const std::vector<unsigned int>& range, const std::vector<unsigned int>& i);
+
+	std::vector<MeshData> GetVertices() const;
+	inline const std::vector<glm::vec3>& GetPositions() const { return m_Positions; }
+	inline const std::vector<glm::vec4>& GetColors() const { return m_Colors; }
+	inline const std::vector<unsigned int>& GetIndices() const { return m_Indices; }
+	inline unsigned int GetNumVertices() const { return m_Positions.size(); }
+
+	void UpdateBuffers();
+
 private:
-    std::vector<MeshData> m_Data;
+	std::vector<glm::vec3> m_Positions;
+	std::vector<glm::vec4> m_Colors;
     std::vector<unsigned int> m_Indices;
+	
     VertexBufferLayout m_VBLayout;
     std::unique_ptr<VertexBuffer> m_VB;
     std::unique_ptr<VertexArray> m_VAO;
     std::unique_ptr<IndexBuffer> m_IB;
-};
 
+	bool m_IsValid() const { return (m_Positions.size() == m_Colors.size()); }
+};
